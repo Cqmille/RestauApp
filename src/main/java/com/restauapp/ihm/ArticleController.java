c85ee3da3255fa455b3fb9e68027ad71f5d52fec
Cqmille <70196927+Cqmille@users.noreply.github.com>
2023-03-22 12:57:10 +0100

    0.31 commit

--- src/main/java/com/restauapp/ihm/ArticleController.java
+++ src/main/java/com/restauapp/ihm/ArticleController.java
@@ -14,16 +14,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class ArticleController {
 
     @Autowired
     RestauService service;
 
-    @GetMapping()
-    public String list(Article article, Model model) {
-        model.addAttribute("avions", service.getAllArticles());
+    @GetMapping("/article")
+    public String list(Model model) {
+        List<Article> articles = service.getAllArticles();
+        List<ArticleForm> articleForms = articles.stream()
+                .map(article -> {
+                    ArticleForm form = new ArticleForm();
+                    form.setId(article.getId());
+                    form.setIntitule(article.getIntitule());
+                    form.setDescription(article.getDescription());
+                    form.setPrix(article.getPrix());
+                    form.setCarteId(article.getCarte().getId());
+                    form.setType(article.getClass().getSimpleName());
+                    return form;
+                })
+                .collect(Collectors.toList());
+        model.addAttribute("articleForms", articleForms);
         return "article";
     }
+
+    // Complete the code the manage CRUD.
+    // Use AvionController as example
 
 }
